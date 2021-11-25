6db5fbf374b9ac9d6013e3dcda3b896ed677be5b
fsahla <fareedulsahlafidha@gmail.com>
2021-11-25 18:36:03 +0530

    first commit

---
+++ SUM.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ int a,b,sum;
+printf("ln enter the first num");
+scanf("%d",&a);
+printf("enter the second num");
+scanf("%d",&b);
+sum=a+b;
+printf("ln sum=%d",sum);
+return(0);
+}
